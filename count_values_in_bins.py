00fc81b0ca2083503af84f14452ee079bfc70620
Manish <dev8manish@gmail.com>
2025-11-25 15:09:16 +0100

    Update count_values_in_bins.py

--- count_values_in_bins.py
+++ count_values_in_bins.py
@@ -1,7 +1,15 @@
 import numpy as np
 
 def count_values_in_bins(data, bin_edges):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    a = len(bin_edges) - 1
+    count = np.zeros(a , dtype = int)
 
+    for i in range(a):
+        if i < a - 1:
+            b = (data >= bin_edges[i]) & (data < bin_edges[i + 1])
+        else:
+            b = (data >= bin_edges[i]) & (data <= bin_edges[i + 1])
+
+        count[i] = np.sum(b)
+
+    return count
