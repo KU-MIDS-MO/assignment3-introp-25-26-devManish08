a001dea9ffe5f1ccddade49ae7a1cc54152fb095
Manish <dev8manish@gmail.com>
2025-11-25 15:09:39 +0100

    Update clean_and_scale_scores.py

--- clean_and_scale_scores.py
+++ clean_and_scale_scores.py
@@ -1,6 +1,12 @@
 import numpy as np
 
 def clean_and_scale_scores(scores, min_score, max_score):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    clean = np.copy(scores)
+
+    clean[clean < min_score] = min_score
+    clean[clean > max_score] = max_score
+
+    scaled = (clean - min_score) / (max_score - min_score)
+
+    return scaled
+
